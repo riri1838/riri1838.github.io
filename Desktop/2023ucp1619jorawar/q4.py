65a871d161a1d5ac92075e83636f6fb6ee2776da
jorawar singh <2023ucp1619@mnit.ac.in>
2024-10-17 11:57:23 +0530

    pythonfile

---
+++ Desktop/2023ucp1619jorawar/q4.py
@@ -0,0 +1,10 @@
+print("enter the vector1")
+print("enter the vector2")
+x1=int(input("enter x1"))
+x2=int(input("enter x2")) 
+x3=int(input("enter x3"))
+
+y1=int(input("enter y1"))
+y2=int(input("enter y2"))
+y3=int(input("enter y3"))
+print(x1*y1+x2*y2+x3*y3)
